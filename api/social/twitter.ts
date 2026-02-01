45bca57b77d8eb543360ca19b1cbb06080908f6c
Bhekie452 <user@example.com>
2026-02-01 20:51:38 +0200

    remove: old API route files that were consolidated to stay within Vercel's function limit

--- api/social/twitter.ts
+++
@@ -1,6 +0,0 @@
-import type { VercelRequest, VercelResponse } from '@vercel/node';
-
-export async function handleTwitter(req: VercelRequest, res: VercelResponse) {
-  const { default: handler } = await import('../../api/twitter');
-  return handler(req, res);
-}
