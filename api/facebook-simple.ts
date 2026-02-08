7972915f3bd95445dca91fd7099f8c16fad2ab41
Bheki Tsabedze <bhekitsabedze452@gmail.com>
2026-02-08 07:36:39 +0200

    Fix Facebook endpoint to accept GET requests
    - Update api/facebook-simple.ts to accept both GET and POST
    - Fix 405 Method Not Allowed error
    - Allow frontend GET requests to work properly
    - Support Facebook pages retrieval via GET method
    - Note: .env file excluded for security

--- api/facebook-simple.ts
+++ api/facebook-simple.ts
@@ -10,7 +10,7 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
     return res.status(200).end();
   }
 
-  if (req.method !== 'POST') {
+  if (req.method !== 'GET' && req.method !== 'POST') {
     return res.status(405).json({ error: 'Method not allowed' });
   }
 
